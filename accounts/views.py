6f8ee9a5da50b922595886c98c98fdbf51061c15
JungAh Choi <108647883+astroastrum@users.noreply.github.com>
2022-10-14 14:43:01 +0900

    회원 목록 조회

--- accounts/views.py
+++ accounts/views.py
@@ -2,36 +2,40 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from .models import User
 
 # Create your views here.
 def index(request):
-    return render(request, 'accounts/index.html')
+    users = User.objects.order_by("-pk")
+    context = {
+        "users": users,
+    }
+
+    return render(request, "accounts/index.html", context)
+
 
 def signup(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('accounts:index')
+            return redirect("accounts:index")
     else:
-        form = CustomUserCreationForm()    
-    context = {
-        'form':form
-    }
-    return render(request, 'accounts/signup.html', context)
+        form = CustomUserCreationForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
+
 
 def login(request):
     if request.user.is_anonymous:
-        if request.method == 'POST':
+        if request.method == "POST":
             form = AuthenticationForm(request, data=request.POST)
             if form.is_valid():
-                auth_login(request, form.get_user()) 
-                return redirect('accounts:index')
+                auth_login(request, form.get_user())
+                return redirect("accounts:index")
         else:
             form = AuthenticationForm()
-        context = {
-            'form': form
-        }
-        return render(request, 'accounts/login.html', context)
+        context = {"form": form}
+        return render(request, "accounts/login.html", context)
     else:
-        return redirect('accounts:index')
+        return redirect("accounts:index")
